9fe59f8b6d74ff2eaece91b1d2236648ff9e14ea
Dmitry Dronov <cskafan1911@gmail.com>
2023-10-14 15:15:52 +0300

    Создан файл file_manager.py. Написаны классы JSONManager, EmployersJSON, VacanciesJSON

---
+++ classes/file_manager.py
@@ -0,0 +1,106 @@
+import ast
+import json
+import os
+from abc import ABC, abstractmethod
+from config import FILENAME_EMPLOYER, FILENAME_VACANCIES
+
+
+class JSONManager(ABC):
+    """
+    Абстрактный класс для работы с файлами JSON
+    """
+
+    @abstractmethod
+    def save_file(self, data):
+        """
+        Метод сохраняет данные в файл формата JSON
+        :param data: Данные для сохранения
+        """
+        pass
+
+    @abstractmethod
+    def read_file(self):
+        """
+        Метод для чтения файла
+        """
+        pass
+
+
+class EmployersJSON(JSONManager):
+    """
+    Класс для работы с файлами работодателя формата json
+    """
+
+    def __init__(self):
+        """
+        Инициализатор для класса EmployersJSON
+        """
+        self.__filename = FILENAME_EMPLOYER + ".json"
+
+    def save_file(self, data: dict):
+        """
+        Метод сохраняет информацию о работодателе в файл формата JSON
+        """
+
+        if os.path.exists(self.__filename):
+            with open(self.__filename, 'r', encoding='utf-8') as file:
+                employer_data = json.load(file)
+            if not any(employer['id'] == data['id'] for employer in employer_data):
+                employer_data.append(data)
+                with open(self.__filename, 'w', encoding='utf-8') as file:
+                    json.dump(employer_data, file, ensure_ascii=False, indent=2)
+
+        else:
+            employer_data = [data]
+
+            with open(self.__filename, 'w', encoding='utf-8') as file:
+                json.dump(employer_data, file, ensure_ascii=False, indent=2)
+
+    def read_file(self):
+        """
+        Метод для чтения файла
+        """
+        with open(self.__filename, 'r', encoding='utf-8') as json_file:
+            json_data = json.load(json_file)
+
+        return json_data
+
+
+class VacanciesJSON(JSONManager):
+    """
+    Класс для работы с файлами вакансий работодателя формата json
+    """
+
+    def __init__(self):
+        """
+        Инициализатор для класса VacanciesJSON
+        """
+        self.__filename = FILENAME_VACANCIES + ".json"
+
+    def save_file(self, data: list[dict]):
+        """
+        Метод сохраняет список вакансий в файл формата JSON
+        """
+
+        if os.path.exists(self.__filename):
+            with open(self.__filename, 'r', encoding='utf-8') as file:
+                vacancy_data = json.load(file)
+            for vacancy_add in data:
+                for vacancy in vacancy_data:
+                    if vacancy['id'] != vacancy_add['id']:
+                        vacancy_data.append(vacancy_add)
+            with open(self.__filename, 'w', encoding='utf-8') as file:
+                json.dump(vacancy_data, file, ensure_ascii=False, indent=2)
+        else:
+            vacancy_data = data
+            with open(self.__filename, 'w', encoding='utf-8') as file:
+                json.dump(vacancy_data, file, ensure_ascii=False, indent=2)
+
+    def read_file(self):
+        """
+        Метод для чтения файла
+        """
+        with open(self.__filename, 'r', encoding='utf-8') as json_file:
+            json_data = json.load(json_file)
+
+        return json_data
