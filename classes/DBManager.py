9c63d1db828e9ff10131aec64c54b2d2d7b3f703
Dmitry Dronov <cskafan1911@gmail.com>
2023-10-19 22:10:06 +0300

    typing в классах DBManager.

--- classes/DBManager.py
+++ classes/DBManager.py
@@ -1,3 +1,7 @@
+from typing import Any
+
+import psycopg2
+
 from classes.DBCreator import DBCreator
 
 
@@ -5,38 +9,61 @@ class DBManager(DBCreator):
     """
     Класс для работы с базами данных Postgres
     """
-    def get_companies_and_vacancies_count(self):
+
+    def get_companies_and_vacancies_count(self) -> list[tuple[Any]]:
         """
         Получает список всех компаний и количество вакансий у каждой компании
-        :return:
         """
-        pass
 
-    def get_all_vacancies(self):
-        """
-        Получает список всех вакансий с указанием названия компании,
-        названия вакансии и зарплаты и ссылки на вакансию
-        :return:
-        """
-        pass
+        return self.__get_any('SELECT employer_name, open_vacancies FROM employers ORDER BY open_vacancies DESC')
 
-    def get_avg_salary(self):
+    def get_all_vacancies(self) -> list[tuple[Any]]:
+        """
+        Получает список всех вакансий с указанием названия компании, названия вакансии и зарплаты (от и до)
+        и ссылки на вакансию
+        """
+
+        return self.__get_any('SELECT employer_name, vacancy_name, salary_from, salary_to, url_vacancy FROM vacancies')
+
+    def get_avg_salary(self) -> list[tuple[Any]]:
         """
         Получает среднюю зарплату по вакансиям
-        :return:
         """
-        pass
 
-    def get_vacancies_with_higher_salary(self):
+        return self.__get_any('SELECT ROUND ((AVG (salary_from) + AVG(salary_to)) / 2) AS AVG_salary FROM vacancies')
+
+    def get_vacancies_with_higher_salary(self) -> list[Any]:
         """
         Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям
         :return:
         """
-        pass
+        result = self.__get_any('SELECT * FROM vacancies WHERE (salary_to + salary_from) / 2 > '
+                                '(SELECT ROUND ((AVG (salary_from) + AVG(salary_to)) / 2) FROM vacancies)')
 
-    def get_vacancies_with_keyword(self):
+        return result
+
+    def get_vacancies_with_keyword(self, word) -> list[tuple[Any]] | str:
         """
         Получает список всех вакансий, в названии которых содержатся переданные в метод слова
         :return:
         """
-        pass
+        conn = psycopg2.connect(dbname=self.database_name, **self.params)
+        with conn.cursor() as cur:
+            cur.execute(
+                "SELECT * FROM vacancies"
+                f" WHERE vacancy_name LIKE '%{word}%'"
+            )
+            rows = cur.fetchall()
+
+        if len(rows) == 0:
+            return f'В названии вакансий из базы данных нет слова {word}'
+
+        return rows
+
+    def __get_any(self, queries) -> list[Any]:
+        conn = psycopg2.connect(dbname=self.database_name, **self.params)
+        with conn.cursor() as cur:
+            cur.execute(queries)
+            rows = cur.fetchall()
+
+        return rows
