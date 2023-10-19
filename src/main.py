4d45c1b72194a2f4db0921bb04b9ee3c352fb0d3
Dmitry Dronov <cskafan1911@gmail.com>
2023-10-19 21:47:33 +0300

    Исправления в функциях user_interaction, main

--- src/main.py
+++ src/main.py
@@ -1,16 +1,13 @@
-import os
-
 from classes.DBCreator import DBCreator
-from classes.DBManager import DBManager
 from classes.file_manager import EmployersJSON, VacanciesJSON
-from src.utils import user_interaction
+from src.utils import user_interaction_one, user_interaction_two
 from config import config
 
 
 def main():
 
     # Получение от пользователя названия компаний для поиска и записи в файлы JSON
-    user_interaction()
+    user_interaction_one()
 
     # Параметры для работы с базами данных Postgres
     params = config()
@@ -29,59 +26,7 @@ def main():
     head_hunter_db.save_employers_to_database(employers)
     head_hunter_db.save_vacancies_to_database(vacancies)
 
-    # Словарь с вариантами запроса
-    query_menu = {
-        0: "0 - Выхода из программы",
-        1: "1 - Список всех компаний и количество вакансий у каждой компании",
-        2: "2 - Список всех вакансий с указанием имени компании, названия вакансии, зарплаты и ссылки на вакансию",
-        3: "3 - Cредняя зарплата по вакансиям",
-        4: "4 - Список всех вакансий, у которых зарплата выше средней по всем вакансиям",
-        5: "5 - Список всех вакансий, по наличию введенного слова"
-    }
-
-    # Функции для ответа на запросы пользователя
-    db_response = DBManager("headhunter_db", params)
-    func_1 = db_response.get_companies_and_vacancies_count()
-    func_2 = db_response.get_all_vacancies()
-    func_3 = db_response.get_avg_salary()
-    func_4 = db_response.get_vacancies_with_higher_salary()
-    func_5 = db_response
-
-    # Словарь с функциями
-    func_dict = {
-        1: func_1, 2: func_2, 3: func_3, 4: func_4, 5: func_5
-    }
-
-    # Счетчик неверных запросов
-    count_error = 3
-
-    while True:
-
-        print('\nВыберите вариант запроса\n')
-        for query in range(0, len(query_menu)):
-            print(f'{query_menu.get(query)}')
-
-        user_number = int(input("Ваш выбор: "))
-
-        if user_number in range(1, 5):
-            print(f'\nВы ввели {query_menu.get(user_number)}')
-            print(f'\n{func_dict.get(user_number)}')
-        elif user_number == 5:
-            user_words = input(f'\nВы ввели {query_menu.get(user_number)}\nВведите ключевые слова: ')
-            print(f'\n{func_dict.get(user_number).get_vacancies_with_keyword(user_words)}')
-        elif user_number == 0:
-            print(f'\nВы ввели {query_menu.get(user_number)}\nДо встречи!!!')
-            break
-        elif count_error != 0:
-            count_error -= 1
-            print(f'Неверный запрос, при 3х неверных запросах программа завершится\n'
-                  f'Осталось неверных запросов: {count_error}\n')
-        else:
-            print('Вы израсходовали максимум неверных запросов(((')
-            break
-
-    os.remove('employers.json')
-    os.remove('vacancies.json')
+    user_interaction_two(params)
 
 
 if __name__ == '__main__':
--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,17 @@
+import os
+
+from classes.DBManager import DBManager
 from classes.file_manager import EmployersJSON, VacanciesJSON
 from classes.head_hunter_API import HeadHunterAPI
 
 
-def user_interaction():
+def user_interaction_one():
     """
     Функция запрашивает у пользователя название компаний и сохраняет их в файлы JSON
     """
     print('Привет! Это программа выдает информацию о 10-ти интересующих работодателях и их открытых вакансиях')
 
-    for num in range(1, 3):
+    for num in range(1, 11):
         user_input = input(f"Введите название компании № {num}:\n")
 
         # Экземпляр класса HeadHunterAPI
@@ -26,3 +29,74 @@ def user_interaction():
         # Экземпляр класса VacanciesJSON
         vacancies_file = VacanciesJSON()
         vacancies_file.save_file(vacancies_data)
+
+
+def user_interaction_two(params):
+    """
+    Функция показывает какую информацию из базы данных можно получить и ждет запроса пользователя
+    """
+    # Словарь с вариантами запроса
+    query_menu = {
+        '0': "0 - Выход из программы",
+        '1': "1 - Список всех компаний и количество вакансий у каждой компании",
+        '2': "2 - Список всех вакансий с указанием имени компании, названия вакансии, зарплаты и ссылки на вакансию",
+        '3': "3 - Cредняя зарплата по вакансиям",
+        '4': "4 - Список всех вакансий, у которых зарплата выше средней по всем вакансиям",
+        '5': "5 - Список всех вакансий, по наличию введенного слова"
+    }
+
+    # Функции для ответа на запросы пользователя
+    db_response = DBManager("headhunter_db", params)
+    func_1 = db_response.get_companies_and_vacancies_count()
+    func_2 = db_response.get_all_vacancies()
+    func_3 = db_response.get_avg_salary()
+    func_4 = db_response.get_vacancies_with_higher_salary()
+    func_5 = db_response
+
+    # Словарь с функциями
+    func_dict = {
+        1: func_1, 2: func_2, 3: func_3, 4: func_4, 5: func_5
+    }
+
+    # Счетчик неверных запросов
+    count_error = 2
+
+    while True:
+
+        print('\nВыберите вариант запроса\n')
+        for query in range(0, len(query_menu)):
+            print(f'{query_menu.get(str(query))}')
+
+        user_number = input("Ваш выбор: ")
+
+        try:
+            if int(user_number) in range(1, 5):
+                print(f'\nВы ввели {query_menu.get(user_number)}')
+                print(f'\n{func_dict.get(int(user_number))}')
+            elif int(user_number) == 5:
+                user_words = input(f'\nВы ввели {query_menu.get(user_number)}\nВведите ключевое слово: ')
+                print(f'\n{func_dict.get(int(user_number)).get_vacancies_with_keyword(user_words)}')
+            elif int(user_number) == 0:
+                print(f'\nВы ввели {query_menu.get(user_number)}\nДо встречи!!!')
+                break
+            elif count_error != 0:
+                print(f'\nНеверный запрос, при 3х неверных запросах программа завершится\n'
+                      f'Осталось неверных запросов: {count_error}')
+                count_error -= 1
+            elif count_error == 0:
+                print('\nВы израсходовали максимум неверных запросов(((\n'
+                      'Программа прекращает работу\n'
+                      'До встречи!!!')
+                break
+        except ValueError:
+            print(f'\nНеверный запрос, при 3х неверных запросах программа завершится\n'
+                  f'Осталось неверных запросов: {count_error}')
+            if count_error == 0:
+                print('\nВы израсходовали максимум неверных запросов(((\n'
+                      'Программа прекращает работу\n'
+                      'До встречи!!!')
+                break
+            count_error -= 1
+
+    os.remove('employers.json')
+    os.remove('vacancies.json')
